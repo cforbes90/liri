28b7e3a2355cc73609b80d5d7d7466517e8902db
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-18 03:07:20 -0400

    Got all major functions working. Next step is to work on the random function

--- liri.js
+++ liri.js
@@ -14,8 +14,8 @@ var spotify = new Spotify(keys.spotify);
 
 console.log("Liri is a command line interface.\nShe has the ability to run the following commands:");
 console.log("1. 'concert-this' followed by  'artist/band name'");
-console.log("2. 'spotify-this-song'");
-console.log("3. 'movie-this'");
+console.log("2. 'spotify-this-song' followed by 'song name'");
+console.log("3. 'movie-this' followed by 'movie title' ");
 console.log("4. 'do-what-it-says' ");
 
 //Capturing user inputs and LIRI commands
@@ -25,7 +25,7 @@ var supportCommand = process.argv.slice(3).join(" ");
 // function concert-this();
 //Sample command node liri.js concert-this <artist/band name here>
 if (prepCommand == "concert-this") {
-    console.log("Your prep command was accepted!");
+    console.log("Your prep command for concert-this was accepted!");
     axios
         .get("https://rest.bandsintown.com/artists/" + supportCommand + "/events?app_id=codingbootcamp")
         .then(function (response) {
@@ -56,38 +56,56 @@ if (prepCommand == "concert-this") {
 if (prepCommand == "spotify-this-song") {
     console.log("You are in Spotify this song!");
     spotify
-        .search({ type: 'track', query: supportCommand, limit: 1 })
+        .search({ type: 'track', query: supportCommand, limit: 5 })
         .then(function (response) {
             // console.log("The following are the artists");
             // console.log(response.tracks.items[0].artists);
             for (let i = 0; i < response.tracks.items.length; i++) {
-                let currentSong = response.tracks.items[i];
+                let song = response.tracks.items[i];
                 let artistsStr = ""
 
-                for (let i = 0; i < currentSong.artists.length; i++) {
+                for (let i = 0; i < song.artists.length; i++) {
                     if (i !== 0) {
-                        artistsStr += ", " + currentSong.artists[i].name;
+                        artistsStr += ", " + song.artists[i].name;
                     } else {
-                        artistsStr = currentSong.artists[i].name;
+                        artistsStr = song.artists[i].name;
                     }
-                    
                 }
                 console.log("--------------");
                 console.log("Artists: ", artistsStr);
-                console.log("Song name: ",currentSong.name);
-                console.log("Song link: ",currentSong.external_urls.spotify);
-                console.log("Album name: ", currentSong.album.name);
+                console.log("Song name: ", song.name);
+                console.log("Song link: ", song.external_urls.spotify);
+                console.log("Album name: ", song.album.name);
                 // console.log(response.tracks);
                 // console.log(response.tracks);
                 console.log("--------------");
             }
-        })    
+        })
         .catch(function (err) {
             console.log(err);
         });
 
 }
+// Sample command node liri.js movie-this '<movie name here>'
 if (prepCommand == "movie-this") {
+    console.log("You are in movie-this!");
+    axios.get("http://www.omdbapi.com/?t=" + supportCommand + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            //   console.log('response:', response)
+            console.log("--------------");
+            console.log("Movie Title:", response.data.Title);
+            console.log("Year Released:", response.data.Year);
+            console.log("IMDB Rating:", response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating:", response.data.Ratings[1].Value);
+            console.log("Production Country:", response.data.Country);
+            console.log("Language:", response.data.Language);
+            console.log("Plot:", response.data.Plot);
+            console.log("Actors:", response.data.Actors);
+            console.log("--------------");
+
+        })
+        .catch(function (error) { })
+
 }
 if (prepCommand == "do-what-it-says") {
 }
